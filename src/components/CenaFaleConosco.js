5d8d18a36fd2372d49bf7ce37ade25ae15e93709
Eduardo Junque <eduardo.junque@navegarti.com.br>
2017-04-10 16:34:49 -0300

    Inclusão do form de fale conosco.

--- src/components/CenaFaleConosco.js
+++ src/components/CenaFaleConosco.js
@@ -13,6 +13,7 @@ const imgBackground = require('../imgs/fdo_user.jpg');
 export default class CenaEditarPerfil extends Component {
   constructor(props){
     super(props);
+    this.state = {msg : ''};
     this.state = {userName : ''};
    }
 
@@ -22,50 +23,64 @@ export default class CenaEditarPerfil extends Component {
       refData.once("value").then((snapshot) => {
         // alert(snapshot.val().name);
         this.setState({ userName: snapshot.val().name});
-
       });
 
     }
 
-  ReturnURL(){
-      return auth.currentUser.photoURL == null ? 'https://s3.amazonaws.com/convertflow/uploads/4e5effb9-0ef6-4975-ad75-1fd20c051e78/NoPhoto_icon-user-default.png' :  auth.currentUser.photoURL;
+ salvarMsg(){
+  if (this.state.msg == null){
+    alert('A mensagem é obrigatória!')
+  } else {
+       const usuarioAtual = auth.currentUser;
+       //Data atual
+       var dateNow = new Date();
+       firebaseRef.child('faleConosco').push().set({
+          userID : usuarioAtual.uid,
+          fcMensagem : this.state.msg,
+          fcData : String(dateNow)
+       });
+       this.setState({msg : ''});
+       alert('Mensagem enviada com sucesso!');
+    }
   }
-
   render() {
     return (
       
         <View style={styles.container}>
-          <View style={styles.topo}>
-            <Topo />
-          </View>
+
           <View style={styles.conteudo}>
-           <Image style={{flex: 1, height: null, width: null, resizeMode: 'cover'}} source= {imgBackground}>
-             <ScrollView>
-                  <View style={{paddingBottom: 30}}>
-                    <View style={styles.formCampos}>
-                      <TextInput
-                      style={styles.formText}
-                      underlineColorAndroid='rgba(0,0,0,0)'
-                      placeholder="Nome"
-                      placeholderTextColor='white'
-                      onChangeText={(name) => this.setState({name})}
-                      value={this.state.name}
-                    />
-                    </View>
+            <Image style={{flex: 1, height: null, width: null, resizeMode: 'cover'}} source= {imgBackground}>
+                <View style={{flex: 1, backgroundColor: 'transparent', alignItems: 'center', marginTop: 80}}>
                   <View>
-                    <TouchableHighlight style={styles.btnCriarConta}
-                        onPress={() => {this.saveUserData(); }}
-                        underlayColor={'#bd0f55'}
-                        activeOpacity={0.5}
-                        >
-                        <Text style={styles.txtCriarConta}>CRIAR CONTA</Text>
-                    </TouchableHighlight>
+                    <Text style={{color: '#e5e5e5', margin: 15, fontSize: 16, marginLeft: 20}}>
+                      Hey {this.state.userName} gostaríamos de sua opinião sobre nosso aplicativo. Só com sua ajuda poderemos continuar melhorando:
+                    </Text>
                   </View>
+                  <View style={styles.formCampos}>
+                    <TextInput
+                    style={styles.formText}
+                    underlineColorAndroid='rgba(0,0,0,0)'
+                    placeholder="Mensagem"
+                    multiline = {true}
+                    numberOfLines = {10}
+                    placeholderTextColor='black'
+                    onChangeText={(msg) => this.setState({msg})}
+                    value={this.state.msg}
+                    maxLength={500}
+                  />
+                  </View>
+                <View>
+                  <TouchableHighlight style={styles.btnFaleconosco}
+                      onPress={() => {this.salvarMsg(); }}
+                      underlayColor={'black'}
+                      activeOpacity={0.5}
+                      >
+                      <Text style={styles.txtFaleConosco}>ENVIAR</Text>
+                  </TouchableHighlight>
                 </View>
-             </ScrollView>
-           </Image>
-         </View>
-         
+              </View>
+            </Image>
+          </View>
           <View style={styles.rodape}>
             <Rodape />
           </View>
@@ -76,25 +91,7 @@ export default class CenaEditarPerfil extends Component {
 }
 
 const styles = StyleSheet.create({
-  viewLogout: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 30,
-    flex: 2
-  },
   viewFaleConosco: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 30,
-    flex: 2
-  },
-  viewRights: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 30,
-    flex: 2
-  },
-  viewUserData: {
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 30,
@@ -102,7 +99,6 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: 'transparent'
   },
   topo: {
     flex: 1.5
@@ -124,5 +120,16 @@ txtFaleConosco: {
   color: '#e5e5e5',
   fontWeight: 'bold',
   fontSize: 14
-}
+},
+formText: {
+  color: 'black',
+  backgroundColor: '#e5e5e5',
+  borderRadius: 15,
+  marginBottom: 20,
+  marginTop: 20,
+  width: 325
+},
+formCampos: {
+  backgroundColor: 'transparent',
+},
 });
